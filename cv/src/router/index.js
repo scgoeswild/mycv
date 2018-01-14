c9b893cf76a244b2576ca33d896c7c01d0a0054f
SImone Chinaglia <hello@simonechinaglia.net>
2018-01-14 14:50:30 +0100

    Chnage router to helloworld due to unit test failure

--- cv/src/router/index.js
+++ cv/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import HelloWorld from '@/components/HelloWorld'
 import Curriculum from '@/components/Curriculum'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'HelloWorld',
+      component: HelloWorld
     },
     {
       path: '/cv',
