0a13cef06af0159b944c19c6f1345481f9561722
SImone Chinaglia <hello@simonechinaglia.net>
2018-01-14 01:07:03 +0100

    fix helloworld

--- cv/src/router/index.js
+++ cv/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/Hello'
+import Hello from '@/components/Hello'
 import Curriculum from '@/components/Curriculum'
 
 Vue.use(Router)
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: HelloWorld
+      component: Hello
     },
     {
       path: '/cv',
